b87477a53a54bf5387514178c64de2d4f6ef1d1f
Tejas Kamble <tejaskambletk132@gmail.com>
2025-01-15 17:57:28 +0530

    Added Login Create and Save project routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-const { signup } = require('../controllers/userControllers');
+const { signup, login, createProj, saveProject } = require('../controllers/userControllers');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,6 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
  router.post("/signup",signup)
-
-
+ router.post("/login",login)
+router.post("/createProj",createProj)
+router.post("/saveProject", saveProject);
 module.exports = router;
