4e9906d0402e1265d7519e86e288493d55688768
Tejas Kamble <tejaskambletk132@gmail.com>
2025-01-16 17:58:40 +0530

    .env added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
-
-
+require('dotenv').config(); // Load environment variables from .env file
 
 const connectDB = async () => {
     try {
-        // Add the database url and name after the cluster URL
-        await mongoose.connect("",{
- 
+        // Use the connection string from the .env file
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        
+
         console.log("MongoDB connected");
     } catch (error) {
         console.log("Error connecting to MongoDB:", error);
